018ebcda6f5ff325c90cce248759d1020297ef7a
Woongbbi <jeon.wbbi@gmail.com>
2019-02-26 22:49:32 +0900

    샘플 코드 수정

--- src/modules/hello_rester/auth.php
+++
@@ -1,9 +0,0 @@
-<?php use rester\sql\session;
-
-if(!defined('__RESTER__')) exit;
-
-$session_id = session::id();
-return array(
-    'title'=>'Passed token check!',
-    'session_id'=>$session_id,
-);
--- src/modules/hello_rester/select_simple.sql
+++
@@ -1,3 +0,0 @@
-SELECT *
-FROM `example`
-LIMIT :start, :rows
